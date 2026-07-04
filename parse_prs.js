19cd519080210ca4ea48064b21430afdfe7e94e8
JiangchenShen <alessandro.qi1234@gmail.com>
2026-07-04 23:27:56 +0200

    Cleanup remaining temporary js merge scripts

--- parse_prs.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const prs = JSON.parse(fs.readFileSync('/tmp/prs.json'));
-if (!Array.isArray(prs)) { console.log(prs); process.exit(1); }
-prs.forEach(pr => {
-  console.log(`PR ${pr.number}: ${pr.title} (branch: ${pr.head.ref})`);
-});
